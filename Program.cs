9e188f767b3b06ac719c5df14921825255cd33bd
SandeepCodeWala <tsandy2210@gmail.com>
2026-05-20 09:15:15 +0530

    New Render Changes

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5266";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
